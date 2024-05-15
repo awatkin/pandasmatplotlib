0a0480da2c8bb9827dd52f85cdc5aeac96ae4040
awatkin <144458571+awatkin@users.noreply.github.com>
2024-05-15 21:24:41 +0100

    Updated the commenting to add docstrings to illustrate the main point of each subroutine.

--- begin.py
+++ begin.py
@@ -10,14 +10,30 @@ def read_data():  # define the subroutine
     return df  # returns the dataframe to where it was requested
 
 
+'''This below function will take the fresh dataframe and 
+print out the contents for the user, but cut down as that is default'''
+
+
 def read_all_data():  # this sub routine will read in a fresh csv file and print out the entire contents
     df = read_data()  # capture the dataframe, fresh one, from read_data into a variable.
     print(df)  # print out the data, will be cut down for clarity in the console.
 
 
+''' The below subroutine WILL take in the data, allow the user
+to select the food item they want and show a graph for the
+food item cross both services that it is available for.
+This is to show how to wrangle data AND show how to set
+series names when they have come from a dataframe like this'''
+
+
 def food_service_graph():  # starts new subroutine to build a graph about item sales.
     df = read_data()  # gets a fresh dataframe in to work with.
     print(df)
+
+
+'''The below subroutine takes in a fresh dataframe and then
+strips out the service to run a total and then outputs
+this data via the console and a matplot lib bar chart'''
 
 
 def total_each_item():  # This sub routine will calculate the totals for each menu item and print them with a graph
@@ -34,19 +50,24 @@ def total_each_item():  # This sub routine will calculate the totals for each me
     plt.show()  # triggers the showing of the plot to the user.
 
 
-def average_each_item():
-    df = read_data()
+'''The below subroutine takes in a fresh dataframe and then
+strips out the service to run a mean / average and then outputs
+this data via the console and a matplot lib bar chart'''
 
-    df1 = df.drop(['Service'], axis=1)
-    df1 = df1.groupby('Menu Item').sum().mean(axis=1)
 
-    print(df1)
+def average_each_item():  # This sub does similar to totals, but average instead
+    df = read_data()  # Creates a fresh, secure, data frame
 
-    print("The item with the highest average is: ", df1.idxmax())
+    df1 = df.drop(['Service'], axis=1)  # drops the service column again as not needed
+    df1 = df1.groupby('Menu Item').sum().mean(axis=1)  # groups data and runs sum and average / mean
 
-    df1.plot.bar()
-    plt.title("Average of Each menu item sales")
-    plt.show()
+    print(df1)  # prints the averages out
+
+    print("The item with the highest average is: ", df1.idxmax())  # makes use of idxmax to give the top average item
+
+    df1.plot.bar()  # Sends data to matplotlib as a bar chart
+    plt.title("Average of Each menu item sales")  # sets the title
+    plt.show()  # shows the chart
 
 
 def main():
